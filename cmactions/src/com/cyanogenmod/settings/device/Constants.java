0ac49f8b1ff834610aace7f8093fc04bde7cb95b
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2017-08-20 05:00:23 -0400

    CMActions: Switch to fingerprint sleep
    Keep it consistant with stock

--- cmactions/src/com/cyanogenmod/settings/device/Constants.java
+++ cmactions/src/com/cyanogenmod/settings/device/Constants.java
@@ -26,16 +26,10 @@ import android.preference.PreferenceManager;
 public class Constants {
 
     // Swap keys
-    public static final String FP_HOME_KEY = "fp_home";
-
-    // Wakeup key
-    public static final String FP_HOME_WAKEUP_KEY = "fp_home_wakeup";
+    public static final String FP_SLEEP_KEY = "fp_sleep";
 
     // Swap nodes
-    public static final String FP_HOME_NODE = "/sys/homebutton/enable";
-
-    // Wakeup node
-    public static final String FP_HOME_WAKEUP_NODE = "/sys/homebutton/enable_wakeup";
+    public static final String FP_SLEEP_NODE = "/sys/bus/spi/devices/spi7.0/key_enable";
 
     // Holds <preference_key> -> <proc_node> mapping
     public static final Map<String, String> sBooleanNodePreferenceMap = new HashMap<>();
@@ -44,15 +38,12 @@ public class Constants {
     public static final Map<String, Object> sNodeDefaultMap = new HashMap<>();
 
     public static final String[] sButtonPrefKeys = {
-        FP_HOME_KEY,
-        FP_HOME_WAKEUP_KEY,
+        FP_SLEEP_KEY,
     };
 
     static {
-        sBooleanNodePreferenceMap.put(FP_HOME_KEY, FP_HOME_NODE);
-        sBooleanNodePreferenceMap.put(FP_HOME_WAKEUP_KEY, FP_HOME_WAKEUP_NODE);
-        sNodeDefaultMap.put(FP_HOME_KEY, false);
-        sNodeDefaultMap.put(FP_HOME_WAKEUP_KEY, false);
+        sBooleanNodePreferenceMap.put(FP_SLEEP_KEY, FP_SLEEP_NODE);
+        sNodeDefaultMap.put(FP_SLEEP_KEY, false);
     }
 
     public static boolean isPreferenceEnabled(Context context, String key) {
